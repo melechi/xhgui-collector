94f48b3f86e9a651f7f8ee9a22cb1066acd703ef
Timothy Chandler <tim@zinios.com>
2019-02-21 12:37:23 +1100

    Fix for latest version of mongo borking on non-encoded keys.

--- src/Xhgui/Saver/Mongo.php
+++ src/Xhgui/Saver/Mongo.php
@@ -21,7 +21,47 @@ class Xhgui_Saver_Mongo implements Xhgui_Saver_Interface
     {
         $data['_id'] = self::getLastProfilingId();
 
-        return $this->_collection->insert($data, array('w' => 0));
+        return $this->insert($data);
+        // return $this->_collection->insert($data, array('w' => 0));
+    }
+    
+    /**
+     * Insert a profile run.
+     *
+     * Does unchecked inserts.
+     *
+     * @param array $profile The profile data to save.
+     */
+    public function insert($profile)
+    {
+        $profile['profile'] = $this->encodeProfile($profile['profile']);
+        return $this->_collection->insert($profile, array('w' => 0));
+    }
+    
+    /**
+     * Encodes a profile to avoid mongodb key errors.
+     * @param array $profile
+     *
+     * @return array
+     */
+    protected function encodeProfile($profile)
+    {
+        if (!is_array($profile)) {
+            return $profile;
+        }
+        $target = array(
+          '__encoded' => true,
+        );
+        foreach ($profile as $k => $v) {
+            if (is_array($v)) {
+                $v = $this->encodeProfile($v);
+            }
+            $replacementKey = strtr($k, array(
+              '.' => '．',
+            ));
+            $target[$replacementKey] = $v;
+        }
+        return $target;
     }
 
     /**
